d21693a55c3ae7f5a4cee411faf9fdd840324359
moyo <moyosorejesuojo@gmail.com>
2023-06-14 20:02:20 +0100

    installed reactjs and tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>something to be cooked</h1>
+    </>
+  )
+}
+
+export default App
