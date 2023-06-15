b5c549487d097a3cb92af33d19561a8668dabcb5
moyo <moyosorejesuojo@gmail.com>
2023-06-15 08:21:33 +0100

    created pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Profile from './pages/Profile'
+import Signup from './pages/Signup'
+import Offers from './pages/Offers'
+import ForgotPassword from './pages/ForgotPassword'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/offers' element={<Offers/>}/>
+        <Route path='/forgotpassword' element={<ForgotPassword/>}/>
+      </Routes>
+    </Router>
       <h1>something to be cooked</h1>
     </>
   )
