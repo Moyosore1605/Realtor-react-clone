200c337cb4a9b05946a7a7239d4b83ef7d73652e
moyo <moyosorejesuojo@gmail.com>
2023-06-30 12:59:34 +0100

    completed the OAuth functionality

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,12 +1,36 @@
-import React from 'react'
-import {FcGoogle} from 'react-icons/fc'
+import React from 'react';
+import {FcGoogle} from 'react-icons/fc';
+import { toast } from 'react-toastify';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import {db} from '../firebase'
+import {doc,getDoc,setDoc,serverTimestamp} from 'firebase/firestore'
+import { useNavigate } from 'react-router-dom';
 
 const OAuth = ()=>{
+    const navigate = useNavigate()
+
+    const onGoogleClick = async()=>{
+        try {
+            const auth = getAuth();
+            const provider = new GoogleAuthProvider();
+            const result = await signInWithPopup(auth,provider);
+            const user = result.user;
+            const docRef = doc(db,'users',user.uid)
+            const docSnap = await getDoc(docRef)
+            if (!docSnap.exists()) {
+                await setDoc(docRef,{name:user.displayName,email:user.email,timeStamp:serverTimestamp()})
+            }
+            toast.success('Signed in successfully with google')
+            navigate('/')
+        } catch (error) {
+            toast.error('Could not sign in with google')
+        }
+    }
+
     return (
-        <button className='flex items-center bg-red-700 px-7 py-3 rounded justify-center w-full text-white text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md
-        hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out'>
-            <FcGoogle className='mr-2 text-2xl bg-white rounded-full'/>
-            CONTINUE WITH GOOGLE</button>
+        <button onClick={onGoogleClick} className='flex items-center bg-red-700 px-7 py-3 rounded justify-center w-full text-white text-sm font-medium hover:bg-red-800 
+        active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out' type='button'>
+            <FcGoogle className='mr-2 text-2xl bg-white rounded-full'/>CONTINUE WITH GOOGLE</button>
     )
 }
 
